fc7bf191ebf48b194173e970aae40a7ae989eb71
EN Sharan <sharanEN27@gmail.com>
2022-08-06 00:58:41 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,23 @@ import Profile from "./pages/profile";
 import Home from "./pages/home";
 import LoggedInRoutes from "./routes/LoggedInRoutes";
 import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
+import { useSelector } from "react-redux";
+import Activate from "./pages/home/activate";
+import Reset from "./pages/reset";
 function App() {
+  const { user } = useSelector((state) => ({ ...state }));
   return (
     <div>
       <Routes>
         <Route element={<LoggedInRoutes />}>
           <Route path="/profile" element={<Profile />} exact />
           <Route path="/" element={<Home />} exact />
+          <Route path="/activate/:token" element={<Activate />} exact />
         </Route>
         <Route element={<NotLoggedInRoutes />}>
           <Route path="/login" element={<Login />} exact />
         </Route>
+        <Route path="/reset" element={<Reset />} />
       </Routes>
     </div>
   );
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,10 +1,24 @@
 import { useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import CreatePost from "../../components/createPost";
 import Header from "../../components/header";
-
+import LeftHome from "../../components/home/left";
+import RightHome from "../../components/home/right";
+import SendVerification from "../../components/home/sendVerification";
+import Stories from "../../components/home/stories";
+import "./style.css";
 export default function Home() {
+  const { user } = useSelector((state) => ({ ...state }));
   return (
-    <div>
+    <div className="home">
       <Header />
+      <LeftHome user={user} />
+      <div className="home_middle">
+        <Stories />
+        {user.verified === false && <SendVerification user={user} />}
+        <CreatePost user={user} />
+      </div>
+      <RightHome user={user} />
     </div>
   );
 }
